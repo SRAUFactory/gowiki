cf679d899b03032f5fe1f451591d036b9cfa0c34
SRAUFactory <srau.factory@gmail.com>
2015-12-15 19:43:53 +0900

    各画面をTemplate化
    「templateパッケージ」の項目を実装

--- wiki.go
+++ wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
@@ -30,7 +30,7 @@ const lenPath = len("/view/")
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[lenPath:]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,12 +39,15 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>Editing %s</h1:>"+
-		"<form action =\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\"> %s</textarea><br>"+
-		"<input type =\"submit\" value=\"Save\">"+
-		"</form>",
-		p.Title, p.Title, p.Body)
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t := template.Must(template.ParseFiles(tmpl + ".html"))
+	err := t.Execute(w, p)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
